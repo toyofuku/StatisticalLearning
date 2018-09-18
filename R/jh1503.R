b2b37758949d10727d52d93acce9d56104765876
toyofuku tsuyoshi <toyofuku.tsuyoshi@gmail.com>
2018-09-18 14:46:52 +0900

    quiver

--- R/jh1503.R
+++ R/jh1503.R
@@ -1,17 +1,17 @@
 rm(list=ls())
-
+library("pracma")
 ###
 ##   Steepest Descent Dynamics
 ###
 ######################## Constants #############################
 AAA <- 5.0
-BBB <- 0.02
-CCC <- 2
+BBB <- 0.02  
+CCC <- 2 
 VECT <- 0.3
 ################################################################
 KURIKAESHI <- 100
 ##################################################
-GRADFIELD <- 0       ### gradient vector
+GRADFIELD <- 1       ### gradient vector 
 OPTIMIZE <- 1        ### optimization
 ##################################################
 #########################################################
@@ -20,30 +20,25 @@ ALPHA <- 0.0         ##### homework     0.0    0.3   0.8
 LANGEVIN <- 0.0      ##### homework     0.0    0.5   1.0
 #########################################################
 X0 <- 1.4            ### initial point
-Y0 <- -2.5           ### initial point
+Y0 <- -2.5           ### initial point 
 ##################################################
 V1 <- seq(-9,9,0.5)
 NNN <- LANGEVIN * sqrt(2.0*ETA)
-
-#[X1 Y1]=meshgrid(V1);
-#[PX1,PY1]=gradient(Z2)
-#contour(V1,V1,Z2,20)
-#if(GRADFIELD==1)
-#  quiver(V1,V1,-VECT*PX1,-VECT*PY1);
-#end
-
-X1 <- sapply(1:37,function(x){return(V1)})
-Y1 <- t(X1)
-
+X1 <- meshgrid(V1,V1)$X
+Y1 <- meshgrid(V1,V1)$Y
 ### E(x,y) = (5sin(x)-y)^2+0.02x^4-2x^2
 Z1 <- AAA * sin(X1) - Y1
 Z2 <- Z1 * Z1 + BBB * X1 ^ 4 - CCC * X1 ^ 2
+PX1 <- gradient(Z2)$X
+PY1 <- gradient(Z2)$Y
 
 par(mfrow=c(1,2))
 
 plot(X0,Y0,xlim=c(-9,9),ylim=c(-9,9), col='red', main='Steepest Descent')
-#contour(V1, V1, Z2, levels=c(20),  drawlabels=FALSE, add=TRUE)
-contour(V1, V1, Z2, add=TRUE)
+contour(V1, V1, t(Z2), add=TRUE, col='black')
+if(GRADFIELD==1){
+  quiver(X1,Y1,-VECT*PX1,-VECT*PY1,col='darkgray')
+}
 
 ###########################################
 
